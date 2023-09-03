297c31b6c5b76763776a54a82ca23b50580be683
ar1oc <ar1oc@yandex.ru>
2023-09-03 18:03:41 +0300

    Изменение заголовкой на админ сайте

--- squad-admin-configurator/squad_admin_configurator/urls.py
+++ squad-admin-configurator/squad_admin_configurator/urls.py
@@ -1,5 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
+admin.site.site_title = settings.ADMIN_SITE_TITLE
+admin.site.index_title = settings.ADMIN_INDEX_TITLE
+admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
     path('admin/', admin.site.urls),
