c1e026f1e6aebc5fe8cd7d47db0fa27b5bf5e384
ar1oc <ar1oc@yandex.ru>
2023-09-03 22:08:29 +0300

    Комментарии, упрощение compose, миграции

--- squad-admin-configurator/start.sh
+++ squad-admin-configurator/start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+echo Wait 5 seconds for database initialization
+sleep 5
+
+echo Start migrations
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate
+
+echo Run application
 gunicorn "squad_admin_configurator.wsgi:application" --bind 0:8000 &
-sleep 10; python3 manage.py cronloop -s 60 > /dev/null &
+echo Run cronloop
+python3 manage.py cronloop -s 300 > /dev/null &
 
 # Wait for any process to exit
 wait -n
