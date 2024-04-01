5c4e94fdbbe65bd89d769506764c6103c6b714b4
ar1ocker <ar1oc@yandex.ru>
2024-04-01 12:53:49 +0300

    Изменение start.sh для отслеживания в логах работы cronloop

--- squad-admin-configurator/start.sh
+++ squad-admin-configurator/start.sh
@@ -8,7 +8,7 @@ python3 manage.py migrate --noinput
 echo Run application
 gunicorn "squad_admin_configurator.wsgi:application" --bind 0:8000 &
 echo Run cronloop
-python3 manage.py cronloop -s 300 > /dev/null &
+python3 manage.py cronloop -s 300 &
 
 # Wait for any process to exit
 wait -n
